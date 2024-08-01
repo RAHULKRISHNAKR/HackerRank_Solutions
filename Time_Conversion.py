6179dca05d1825c75de3768e1bbc44cde2331118
Rahul Krishna K R <rahulkridhna@gmail.com>
2024-08-01 18:58:45 +0530

    Create Time_Conversion.py

---
+++ Time_Conversion.py
@@ -0,0 +1,43 @@
+'''
+Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    is_pm = s[-2:]=='PM'
+    hr,mint,sec= map(int,s[:-2].split(':'))
+    if is_pm and hr!=12:
+        hr+=12
+    elif not is_pm and hr==12:
+        hr=0
+    return f"{hr:02d}:{mint:02d}:{sec:02d}"
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
