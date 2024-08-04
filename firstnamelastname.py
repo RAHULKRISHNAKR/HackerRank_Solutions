db0006441071393c513fd248fd1699831dd9ab5c
Rahul Krishna K R <rahulkridhna@gmail.com>
2024-08-04 19:05:41 +0530

    Create firstnamelastname.py

---
+++ firstnamelastname.py
@@ -0,0 +1,12 @@
+#You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+
+#Hello firstname lastname! You just delved into python.
+
+def print_full_name(first, last):
+    # Write your code here
+    string="Hello "
+    string+=first
+    string+=" "
+    string+=last
+    string+="! You just delved into python."
+    print(string)
